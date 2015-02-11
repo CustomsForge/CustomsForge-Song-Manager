e1fa18a5063b736f9c6e8c1f8722626d0bc844b8
Lovroman <pamet.pijelova@gmail.com>
2015-02-11 16:41:03 +0100

    Header column added.

--- CustomsForgeManager_Winforms/Forms/frmMain.cs
+++ CustomsForgeManager_Winforms/Forms/frmMain.cs
@@ -218,8 +218,8 @@ namespace CustomsForgeManager_Winforms.Forms
             {
                 path = sfdSongListToCSV.FileName;
             }
-            sbCSV.Append(@"sep=;");
-            sbCSV.Append(Environment.NewLine);
+            sbCSV.AppendLine(@"sep=;");
+            sbCSV.AppendLine("Artist;Song;Album;Year;Tuning;Arrangements");
             foreach (var song in SongCollection)
             {
                 sbCSV.AppendLine(song.Artist + ";" + song.Song + ";" + song.Album + ";" + song.SongYear + ";" + song.Tuning + ";" + song.Arrangements);
