9fdd1f83ffd68bf1ebf3d12196ddf27fd194244c
daredev <piczok@gmail.com>
2015-09-09 14:54:13 +0200

    -Added scrollbar to Release Notes -Published v.1.0.0.6

--- CustomsForgeManager/Forms/frmNoteViewer.Designer.cs
+++ CustomsForgeManager/Forms/frmNoteViewer.Designer.cs
@@ -38,6 +38,7 @@
             this.rtbNotes.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.rtbNotes.Location = new System.Drawing.Point(0, 0);
             this.rtbNotes.Name = "rtbNotes";
+            this.rtbNotes.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.ForcedVertical;
             this.rtbNotes.Size = new System.Drawing.Size(621, 450);
             this.rtbNotes.TabIndex = 1;
             this.rtbNotes.Text = "This is a Rich Textbox Note Viewer";
