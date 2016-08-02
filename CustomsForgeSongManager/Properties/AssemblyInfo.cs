6d06bbaa10dfc864e97fc5227f840df476b2220f
Lovroman <pamet.pijelova@gmail.com>
2016-08-02 09:05:50 +0200

    1.2.3.5
    -ODLC list update

--- CustomsForgeSongManager/Properties/AssemblyInfo.cs
+++ CustomsForgeSongManager/Properties/AssemblyInfo.cs
@@ -36,5 +36,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.2.3.4")]
-[assembly: AssemblyFileVersion("1.2.3.4")]
+[assembly: AssemblyVersion("1.2.3.5")]
+[assembly: AssemblyFileVersion("1.2.3.5")]
